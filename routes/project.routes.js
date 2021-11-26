9029e0489b07efde5584d03b46282ab37c1dd8cb
roman-bianko <skayone1@gmail.com>
2021-11-26 13:56:52 +0300

    create project api

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const Project = require('../models/Project')
+const User = require('../models/User')
 const router = Router()
 
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT, POST, OPTIONS");
     next();
 });
 
@@ -21,7 +23,7 @@ router.post('/create', async (req, res) => {
             projectPicture,
             requests,
             devs
-        } = req.body
+        } = req.body.params.project
 
         const project = new Project({
             title,
@@ -33,7 +35,8 @@ router.post('/create', async (req, res) => {
             requests,
             devs
         })
-
+        const creatorId = devs[0]._id
+        await User.updateOne({ _id: creatorId }, { $push: { projects: project._id.toString() } })
         await project.save()
         res.status(201).json({ message: 'Project created!' })
     } catch (error) {
@@ -44,7 +47,13 @@ router.post('/create', async (req, res) => {
 // api/projects/list
 router.get('/list', async (req, res) => {
     try {
-        const projects = await Project.find()
+        let projects = []
+        if (req.query.ids) {
+            const idsArray = req.query.ids
+            projects = await Project.find({ _id: { $in: idsArray } }, { __v: 0 })
+        } else {
+            projects = await Project.find({}, { __v: 0 })
+        }
         res.json(projects)
     } catch (error) {
         res.status(500).json({ message: error.message })
